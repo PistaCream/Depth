81c690296e1719d273087284662f7a492b2b8126
baekanthony <baekanthony@gmail.com>
2025-12-02 21:59:42 -0800

    pause reset buttons for timer

--- app/src/main/java/com/example/depth/presentation/timer/TimerScreen.kt
+++ app/src/main/java/com/example/depth/presentation/timer/TimerScreen.kt
@@ -3,10 +3,17 @@ package com.example.depth.presentation.timer
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Button
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material.icons.filled.Pause
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -19,13 +26,15 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import java.util.Timer
 import kotlin.concurrent.fixedRateTimer
 
 //TODO: duration should be configurable
-const val TIMER_DURATION_SECONDS = 5
+const val TIMER_DURATION_SECONDS = 10
 
 @Composable
 fun TimerScreen(timerViewModel: TimerViewModel) {
+    var timer by remember { mutableStateOf<Timer?>(null) }
     var startTimer by remember { mutableStateOf(false) }
     var timerProgressSeconds by remember { mutableIntStateOf(0) }
 
@@ -34,8 +43,9 @@ fun TimerScreen(timerViewModel: TimerViewModel) {
     }
 
     LaunchedEffect(startTimer) {
+        timer?.cancel()
         if (startTimer) {
-            fixedRateTimer(period = 1000L) {
+            timer = fixedRateTimer(period = 1000L) {
                 timerProgressSeconds++
                 if (timerProgressSeconds >= TIMER_DURATION_SECONDS) {
                     cancel()
@@ -59,9 +69,32 @@ fun TimerScreen(timerViewModel: TimerViewModel) {
         Box(contentAlignment = Alignment.Center) {
             DeterminateCircularProgress(progress = timerProgressSeconds, modifier = Modifier.size(270.dp))
             Text(text = getRemainingTime(TIMER_DURATION_SECONDS - timerProgressSeconds))
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+            ) {
+                Spacer(Modifier.height(100.dp))
+                IconButton(onClick = {
+                    timer?.cancel()
+                    startTimer = false
+                    timerProgressSeconds = 0
+                }) {
+                    Icon(Icons.Default.Refresh, "Reset timer")
+                }
+            }
         }
-        Button(onClick = { startTimer = true }) {
-            Text("Start")
+        if (startTimer && timerProgressSeconds < TIMER_DURATION_SECONDS) {
+            IconButton(onClick = {
+                startTimer = false
+            }) {
+                Icon(Icons.Default.Pause, "Pause timer")
+            }
+        } else if (timerProgressSeconds < TIMER_DURATION_SECONDS) {
+            IconButton(onClick = {
+                startTimer = true
+            }) {
+                Icon(Icons.Default.PlayArrow, "Start timer")
+            }
         }
     }
 }
